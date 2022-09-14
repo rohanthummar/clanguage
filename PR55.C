866ced38b6404f09c103dcbf8cbf18042c25ff9f
rohanthummar <rohanthummar25@gmail.com>
2022-09-14 12:30:07 +0100

    clanguage11

---
+++ PR55.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=5,b=10;
+clrscr();
+       ++a;
+       printf("sum of a %d\n",a);
+       --b;
+       printf("sub of b %d\n",b);
+
+
+
+
+getch();
+}
